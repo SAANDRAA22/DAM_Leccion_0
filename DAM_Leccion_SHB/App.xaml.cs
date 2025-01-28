f37958ac781a378507572cc2a2562cafacdda9ad
SAANDRAA22 <20030178@itesa.edu.mx>
2025-01-28 17:52:41 -0600

    Versiòn_01: Se creô el proyecto MAUI vacìo.

---
+++ DAM_Leccion_SHB/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DAM_Leccion_SHB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ DAM_Leccion_SHB/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DAM_Leccion_SHB
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
