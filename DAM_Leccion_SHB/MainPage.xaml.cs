782f16146b38c7641dd3d2a74bf9b17ee8f8f308
SAANDRAA22 <20030178@itesa.edu.mx>
2025-01-28 21:12:48 -0600

    Version_03: Hasta la BD ya sin error

--- DAM_Leccion_SHB/MainPage.xaml.cs
+++ DAM_Leccion_SHB/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_SHB
+﻿using DAM_Leccion_SHB.Model;
+
+namespace DAM_Leccion_SHB
 {
     public partial class MainPage : ContentPage
     {
@@ -7,18 +9,58 @@
         public MainPage()
         {
             InitializeComponent();
+            //Ejecutar();
+
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnCounterClicked(object sender, EventArgs e )
         {
             count++;
-
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
-
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+
+        {
+            //{
+            //PersonaModel personaModel = new PersonaModel()
+
+            //{
+
+            //txtNombre.Text = "Hola aqui estoy";
+            //}
+
+            //BindingContext = personaModel.Nombre;
+
+            PersonaModel personaModel = new PersonaModel()
+
+            {
+                Nombre = "Hola aqui estoy",
+            };
+
+            //personaModel.Nombre = "Hola aqui estoy";
+
+            //txtNombre.Text = personaModel.Nombre;
+
+
+
+
+            Binding personaBinding = new Binding();
+
+            personaBinding.Source = personaModel; //Origen
+            personaBinding.Path = "Nombre"; //ruta
+            txtNombre.SetBinding(Entry.TextProperty, personaBinding);
+
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+
+            DisplayAlert("Asistente del sistema", "Se ha guardado el registro en la BD", "Aceptar");
         }
     }
 
---
+++ DAM_Leccion_SHB/Model/PersonaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM_Leccion_SHB.Model
+{
+    public class PersonaModel
+    {
+        public string Nombre { get; set; }
+    }
+}
